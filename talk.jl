cabe529a53f3531a9b57ab3f908715340633e51a
Michael Tiemann <github@michaeltiemann.net>
2024-03-11 23:58:57 +0100

    add links to references

---
+++ talk.jl
@@ -0,0 +1,27 @@
+### A Pluto.jl notebook ###
+# v0.19.40
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 83b9b994-dff7-11ee-1a06-8fc07b2e8b04
+md"""
+# Language 1.5 revisited
+
+*Michael Tiemann*
+"""
+
+# ╔═╡ 00982b72-a1e4-4ce0-8fc7-1d147ce1ffb0
+md"""
+## References
+
+- [Julia discourse](https://discourse.julialang.org/t/does-julia-create-a-1-5-language-problem/107984)
+- [Reddit thread](https://www.reddit.com/r/Julia/comments/18nyxew/michael_tiemann_julia_solves_the_2_language/)
+- [Reddit follow-up thread](https://www.reddit.com/r/Julia/comments/1abenv8/2024_maybe_the_year_for_julia/?rdt=59504)
+- [YouTube video](https://www.youtube.com/watch?v=RUJFd-rEa0k)
+- [Google discussion](https://groups.google.com/g/julia-users/c/pD0qzv_owC8)
+"""
+
+# ╔═╡ Cell order:
+# ╟─83b9b994-dff7-11ee-1a06-8fc07b2e8b04
+# ╠═00982b72-a1e4-4ce0-8fc7-1d147ce1ffb0

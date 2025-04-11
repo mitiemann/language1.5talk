3837e1f8408ea884c9fffbc0440cc09dafea078e
Michael Tiemann <github@michaeltiemann.net>
2025-04-11 11:50:38 +0200

    update slides to almost JuliaCon version

--- talk.jl
+++ talk.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.43
+# v0.20.5
 
 using Markdown
 using InteractiveUtils
@@ -23,9 +23,11 @@ md"""
 $(LocalResource("imgs/michael.png"))
 """
 
-# ╔═╡ 74f6257c-356a-4e7b-b701-a350fc6d83a0
-md"""
-# Audience participation
+# ╔═╡ 349564ee-b6a4-408f-a98b-fea1f0f8066a
+md"""# 
+
+$(LocalResource("imgs/julia-paper-title.png"))
+$(LocalResource("imgs/julia-paper-text.png"))
 """
 
 # ╔═╡ bf0829ce-8d26-4d4b-a165-8532724e0337
@@ -40,27 +42,15 @@ md"""
 
 # ╔═╡ 5432b132-b390-4340-ba26-7a237bfdfd0b
 md"""
-# 
+# The core hypothesis statement
+
+!!! info "The 1.5 language problem"
+
+	The common expression "Julia solves the two language problem" creates an *expectation* in newcomers which is not **trivially** satisfied.
+
 !!! question "Note"
 
 	This talk is concerned with _technical aspects_ of the Julia programming language, but presents opinions on the _Julia user experiences_.
-"""
-
-# ╔═╡ 24833199-16fd-4c35-b901-a7f7cafa2ec4
-md"""
-# The two language problem
-
-> "Two-tiered architectures have emerged as the standard compromise between convenience and performance: programmers express high-level logic in a dynamic language while the heavy lifting is done in C and Fortran."
-Jeff Bezanson, Stefan Karpinski, Viral B. Shah, Alan Edelman: "Julia: A Fast Dynamic Language for Technical Computing". 2012
-"""
-
-# ╔═╡ 31af4b5e-31a3-419a-bccc-a6837afa19c7
-md"""
-_probably delete_
-
-## Summary of online discussion points
-
-- TODO
 """
 
 # ╔═╡ 8327ee17-f6e9-451c-9c52-7ec45253fac7
@@ -76,28 +66,48 @@ md"""
 
 $(LocalResource("imgs/rabbits.png", :width => 350))
 $(LocalResource("imgs/cheetah.png", :width => 300))
-Rapid prototyping vs. rapid execution
+Rapid prototyping (left), rapid execution (right)
 """
 
 # ╔═╡ f2b73970-1e3e-4363-8108-2de146b04bac
 md"""
 # Naive expectation
-$(LocalResource("imgs/plot-01.jpeg"))
+
+$(LocalResource("imgs/1d-understanding.png"))
+"""
+
+# ╔═╡ 7bf2b6ac-1f10-41d3-a3b5-54b67b49c2c2
+md"""
+# Naive expectation
+
+$(LocalResource("imgs/julia-benchmark.png"))
 """
 
 # ╔═╡ 1e4b1c08-6023-4a0a-9f58-7bd20af55733
 md"""
 # Realistic expectation
 
-$(LocalResource("imgs/plot-02.jpeg"))
-Julia code can be & _will be_ slow in rapid prototyping
+$(LocalResource("imgs/1d-revisited.png"))
+
+!!! info "Corollary: the new language problem"
+
+	You can write slow and messy code in any language,
+	and will do so when you‘re new.
+"""
+
+# ╔═╡ 343bb1da-9eff-4586-a123-bc3a3406665f
+md"""
+# 
+
+$(LocalResource("imgs/liam.png"))
+[Liam Hurwitz: Julia is the Answer, but what is the question? *JuliaCon Local Eindhoven 2023*](https://www.youtube.com/watch?v=LW8kuoELgC4)
 """
 
 # ╔═╡ 12d68f35-f5d1-46f0-af53-25bd95eac780
 md"""
 # It's not a bug, it's a feature
 
-$(LocalResource("imgs/plot-03.jpeg"))
+$(LocalResource("imgs/2d-understanding.png"))
 """
 
 # ╔═╡ 660a6e5b-3271-48e2-89ea-f5ce0c260289
@@ -106,74 +116,110 @@ md"""
 The pareto front has a non-integer Hausdorff dimension ;-)
 
 $(Resource("https://upload.wikimedia.org/wikipedia/commons/b/bc/Great_Britain_Hausdorff.svg"))
-(Image credit: By Prokofiev - Own work, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=12042048)
-"""
-
-# ╔═╡ 343bb1da-9eff-4586-a123-bc3a3406665f
-md"""
-$(LocalResource("imgs/liam.png"))
-[Liam Hurwitz: Julia is the Answer, but what is the question? *JuliaCon Local Eindhoven 2023*](https://www.youtube.com/watch?v=LW8kuoELgC4)
-"""
-
-# ╔═╡ 49522e07-b056-438a-aaf0-8c34995cefca
-md"""
-## The 2 language problem is the multi-dimensional Pareto front language problem
-
-- one dimension: ease of rapid prototyping
-- one dimension: achievable top speed
-- one dimension: ease of deployability
-- ... and many more dimensions (cf. [Liam's talk](https://youtu.be/LW8kuoELgC4?si=GytLdoRWXzICHZfk))
+(Image credit: [By Prokofiev - Own work, CC BY-SA 3.0](https://commons.wikimedia.org/w/index.php?curid=12042048))
 """
 
 # ╔═╡ a3577fdc-dc1f-4df0-b692-978f57c1a897
 md"""
-## Happyness is an illusion
+# Happyness is an illusion
 
-$(LocalResource("imgs/plot-04.jpeg"))
+$(LocalResource("imgs/2d-revisited.png"))
 """
 
 # ╔═╡ e8ff10e9-8892-4096-800f-3a7e35ca5943
 md"""
-## Whatever the problem, what's the **solution**?
+#
 
-- Most learning materials target the rapid prototyping dimension (YMMV)
-  + Some exceptions: [DiffEq's tutorial](https://docs.sciml.ai/DiffEqDocs/stable/tutorials/faster_ode_example/), [Julia High Performance book](https://juliahighperformance.com/), [this website](https://enccs.github.io/julia-for-hpc/), [this repo](https://github.com/carstenbauer/JuliaHLRS22)
-- Newcomers need some additional information about the *context* of  available (learning) material, i.e., is the goal of a package/tutorial/... development speed or execution speed?
+$(LocalResource("imgs/problems-solutions-meme.png"))
 """
 
 # ╔═╡ 3a1bef2b-1843-4caa-9c09-9f17bd283e03
 md"""
-## An open-source book about iterative refinement from rapid prototyping to rapid execution code
+# Best practices
 
-*the problematic recipe: here's all the ingredients, the final product should look like this*
+$(LocalResource("imgs/books.png"))
+**TODO** Add links to each book
 
-*julia.kitchen*
+!!! info "Related JuliaCon talk"
+	julia.kitchen\
+	Jorge Vieyra\
+	7-12, 2:00-2:30, REPL
 """
 
 # ╔═╡ 1521ea0d-b6f9-4b7c-8b9d-2c1cd3cb492b
+# ╠═╡ disabled = true
+#=╠═╡
 md"""
 # Call to action: live stream your refactoring sessions
 
 *I want you, PhD students, for Julia tutorials*
 """
+  ╠═╡ =#
 
 # ╔═╡ 92639f81-e406-4a55-8812-66ec01d29b16
 md"""
-## A visual guide for expectation management
+# A visual guide for expectation management
 
-*this needs to come much earlier*
+$(LocalResource("imgs/rabbits.png", :width => 400))
+$(LocalResource("imgs/cheetah.png", :width => 200))
+"""
+
+# ╔═╡ b0090f60-95d0-4f12-ab6f-6c74a636f686
+md"""
+# A visual guide for expectation management
+
+$(LocalResource("imgs/rabbits.png", :width => 200))
+$(LocalResource("imgs/cheetah.png", :width => 400))
 """
 
 # ╔═╡ aa640f8d-791a-4c28-a3e9-c5cfae8ed7e7
 md"""
-## Outro
+# I want YOU
 
-- Summary
-- Feedback welcome
-- Repeat QR code
+$(LocalResource("imgs/uncle-sam.jpg", :width => 150))
+- Write learning material and documentation
+- Offer online pair programming
+- "Request a senior reviewer" service
+"""
+
+# ╔═╡ 8b406501-0c30-40e8-b5fd-dc8912f4a9cb
+md"""
+# ... also when you failed
+
+$(Resource("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Survivorship-bias.svg/960px-Survivorship-bias.svg.png?20221011173327"))
+(Image credit: [Martin Grandjean (vector), McGeddon (picture), US Air Force (hit plot concept), CC BY-SA](https://commons.wikimedia.org/wiki/File:Survivorship-bias.svg) )
 """
 
 # ╔═╡ b58a68aa-4709-4c1f-b680-dfe434679a04
+md"""
+# Have fun!
+
+**TODO** Add GenAI picture of "have fun!"
+"""
+
+# ╔═╡ a8e876a7-4881-4b2a-87fd-6471c35dffd9
+md"""
+# Fun technology
+
+$(LocalResource("imgs/fun-tech-1.png"))
+$(LocalResource("imgs/fun-tech-2.png"))
+"""
+
+# ╔═╡ 9e431fda-2f68-48dd-8d6d-ea08f0eb28bc
+md"""
+# Julia fun technology
+
+$(LocalResource("imgs/julia-fun-tech.png"))
+"""
+
+# ╔═╡ 2dd5b6a3-8146-460d-9dd1-31c34bf6e2f8
+md"""
+# Have fun!
+
+**TODO** Add same GenAI picture of "have fun!"
+"""
+
+# ╔═╡ 5c4ed95d-0119-4d06-b6b2-378169029bcd
 md"""
 # Appendix
 """
@@ -188,9 +234,9 @@ md"""
 - [YouTube video](https://www.youtube.com/watch?v=RUJFd-rEa0k)
 - [Google discussion](https://groups.google.com/g/julia-users/c/pD0qzv_owC8)
 
-_TODO_
+**TODO**
 
-- SciML book and Chris Rackauckas video lectures
+- Chris Rackauckas video lectures
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
@@ -206,9 +252,9 @@ PlutoUI = "~0.7.59"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.10.4"
+julia_version = "1.11.4"
 manifest_format = "2.0"
-project_hash = "6e7bcec4be6e95d1f85627422d78f10c0391f199"
+project_hash = "4a39792926115186a2794d75b313cd5d293ecdf7"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -218,13 +264,15 @@ version = "1.3.2"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
-version = "1.1.1"
+version = "1.1.2"
 
 [[deps.Artifacts]]
 uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+version = "1.11.0"
 
 [[deps.Base64]]
 uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+version = "1.11.0"
 
 [[deps.ColorTypes]]
 deps = ["FixedPointNumbers", "Random"]
@@ -240,6 +288,7 @@ version = "1.1.1+0"
 [[deps.Dates]]
 deps = ["Printf"]
 uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+version = "1.11.0"
 
 [[deps.Downloads]]
 deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
@@ -248,6 +297,7 @@ version = "1.6.0"
 
 [[deps.FileWatching]]
 uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
+version = "1.11.0"
 
 [[deps.FixedPointNumbers]]
 deps = ["Statistics"]
@@ -276,6 +326,7 @@ version = "0.2.5"
 [[deps.InteractiveUtils]]
 deps = ["Markdown"]
 uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+version = "1.11.0"
 
 [[deps.JSON]]
 deps = ["Dates", "Mmap", "Parsers", "Unicode"]
@@ -291,16 +342,17 @@ version = "0.6.4"
 [[deps.LibCURL_jll]]
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
-version = "8.4.0+0"
+version = "8.6.0+0"
 
 [[deps.LibGit2]]
 deps = ["Base64", "LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
 uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+version = "1.11.0"
 
 [[deps.LibGit2_jll]]
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll"]
 uuid = "e37daf67-58a4-590a-8e99-b0245dd2ffc5"
-version = "1.6.4+0"
+version = "1.7.2+0"
 
 [[deps.LibSSH2_jll]]
 deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
@@ -309,34 +361,39 @@ version = "1.11.0+1"
 
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+version = "1.11.0"
 
 [[deps.LinearAlgebra]]
 deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
 uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+version = "1.11.0"
 
 [[deps.Logging]]
 uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+version = "1.11.0"
 
 [[deps.MIMEs]]
-git-tree-sha1 = "65f28ad4b594aebe22157d6fac869786a255b7eb"
+git-tree-sha1 = "c64d943587f7187e751162b3b84445bbbd79f691"
 uuid = "6c6e2e6c-3030-632d-7369-2d6c69616d65"
-version = "0.1.4"
+version = "1.1.0"
 
 [[deps.Markdown]]
 deps = ["Base64"]
 uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+version = "1.11.0"
 
 [[deps.MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
-version = "2.28.2+1"
+version = "2.28.6+0"
 
 [[deps.Mmap]]
 uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+version = "1.11.0"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
-version = "2023.1.10"
+version = "2023.12.12"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
@@ -345,7 +402,7 @@ version = "1.2.0"
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
-version = "0.3.23+4"
+version = "0.3.27+1"
 
 [[deps.Parsers]]
 deps = ["Dates", "PrecompileTools", "UUIDs"]
@@ -354,15 +411,21 @@ uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
 version = "2.8.1"
 
 [[deps.Pkg]]
-deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "Random", "SHA", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
-version = "1.10.0"
+version = "1.11.0"
+
+    [deps.Pkg.extensions]
+    REPLExt = "REPL"
+
+    [deps.Pkg.weakdeps]
+    REPL = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
 
 [[deps.PlutoUI]]
 deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
-git-tree-sha1 = "ab55ee1510ad2af0ff674dbcced5e94921f867a9"
+git-tree-sha1 = "d3de2694b52a01ce61a036f18ea9c0f61c4a9230"
 uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
-version = "0.7.59"
+version = "0.7.62"
 
 [[deps.PrecompileTools]]
 deps = ["Preferences"]
@@ -379,14 +442,12 @@ version = "1.4.3"
 [[deps.Printf]]
 deps = ["Unicode"]
 uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
-
-[[deps.REPL]]
-deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
-uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+version = "1.11.0"
 
 [[deps.Random]]
 deps = ["SHA"]
 uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+version = "1.11.0"
 
 [[deps.Reexport]]
 git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
@@ -399,24 +460,19 @@ version = "0.7.0"
 
 [[deps.Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
-
-[[deps.Sockets]]
-uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
-
-[[deps.SparseArrays]]
-deps = ["Libdl", "LinearAlgebra", "Random", "Serialization", "SuiteSparse_jll"]
-uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
-version = "1.10.0"
+version = "1.11.0"
 
 [[deps.Statistics]]
-deps = ["LinearAlgebra", "SparseArrays"]
+deps = ["LinearAlgebra"]
+git-tree-sha1 = "ae3bb1eb3bba077cd276bc5cfc337cc65c3075c0"
 uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
-version = "1.10.0"
+version = "1.11.1"
 
-[[deps.SuiteSparse_jll]]
-deps = ["Artifacts", "Libdl", "libblastrampoline_jll"]
-uuid = "bea87d4a-7f5b-5778-9afe-8cc45184846c"
-version = "7.2.1+1"
+    [deps.Statistics.extensions]
+    SparseArraysExt = ["SparseArrays"]
+
+    [deps.Statistics.weakdeps]
+    SparseArrays = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
 
 [[deps.TOML]]
 deps = ["Dates"]
@@ -431,23 +487,26 @@ version = "1.10.0"
 [[deps.Test]]
 deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
 uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+version = "1.11.0"
 
 [[deps.Tricks]]
-git-tree-sha1 = "eae1bb484cd63b36999ee58be2de6c178105112f"
+git-tree-sha1 = "6cae795a5a9313bbb4f60683f7263318fc7d1505"
 uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
-version = "0.1.8"
+version = "0.1.10"
 
 [[deps.URIs]]
-git-tree-sha1 = "67db6cc7b3821e19ebe75791a9dd19c9b1188f2b"
+git-tree-sha1 = "cbbebadbcc76c5ca1cc4b4f3b0614b3e603b5000"
 uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
-version = "1.5.1"
+version = "1.5.2"
 
 [[deps.UUIDs]]
 deps = ["Random", "SHA"]
 uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+version = "1.11.0"
 
 [[deps.Unicode]]
 uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+version = "1.11.0"
 
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
@@ -457,12 +516,12 @@ version = "1.2.13+1"
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.8.0+1"
+version = "5.11.0+0"
 
 [[deps.nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
-version = "1.52.0+1"
+version = "1.59.0+0"
 
 [[deps.p7zip_jll]]
 deps = ["Artifacts", "Libdl"]
@@ -474,26 +533,30 @@ version = "17.4.0+2"
 # ╟─533d82d7-8c4e-4b43-9628-ca779b9c0b50
 # ╟─83b9b994-dff7-11ee-1a06-8fc07b2e8b04
 # ╟─9adc5a36-51bc-4a1c-8d47-3bbf05eae26f
-# ╟─74f6257c-356a-4e7b-b701-a350fc6d83a0
+# ╟─349564ee-b6a4-408f-a98b-fea1f0f8066a
 # ╟─bf0829ce-8d26-4d4b-a165-8532724e0337
 # ╟─5432b132-b390-4340-ba26-7a237bfdfd0b
-# ╟─24833199-16fd-4c35-b901-a7f7cafa2ec4
-# ╟─31af4b5e-31a3-419a-bccc-a6837afa19c7
 # ╟─8327ee17-f6e9-451c-9c52-7ec45253fac7
 # ╟─b6278db3-8994-4e0b-b331-3df55cad188e
-# ╠═f2b73970-1e3e-4363-8108-2de146b04bac
-# ╠═1e4b1c08-6023-4a0a-9f58-7bd20af55733
-# ╠═12d68f35-f5d1-46f0-af53-25bd95eac780
+# ╟─f2b73970-1e3e-4363-8108-2de146b04bac
+# ╟─7bf2b6ac-1f10-41d3-a3b5-54b67b49c2c2
+# ╟─1e4b1c08-6023-4a0a-9f58-7bd20af55733
+# ╟─343bb1da-9eff-4586-a123-bc3a3406665f
+# ╟─12d68f35-f5d1-46f0-af53-25bd95eac780
 # ╟─660a6e5b-3271-48e2-89ea-f5ce0c260289
-# ╠═343bb1da-9eff-4586-a123-bc3a3406665f
-# ╟─49522e07-b056-438a-aaf0-8c34995cefca
-# ╠═a3577fdc-dc1f-4df0-b692-978f57c1a897
-# ╠═e8ff10e9-8892-4096-800f-3a7e35ca5943
-# ╠═3a1bef2b-1843-4caa-9c09-9f17bd283e03
+# ╟─a3577fdc-dc1f-4df0-b692-978f57c1a897
+# ╟─e8ff10e9-8892-4096-800f-3a7e35ca5943
+# ╟─3a1bef2b-1843-4caa-9c09-9f17bd283e03
 # ╠═1521ea0d-b6f9-4b7c-8b9d-2c1cd3cb492b
-# ╠═92639f81-e406-4a55-8812-66ec01d29b16
-# ╠═aa640f8d-791a-4c28-a3e9-c5cfae8ed7e7
-# ╠═b58a68aa-4709-4c1f-b680-dfe434679a04
-# ╠═00982b72-a1e4-4ce0-8fc7-1d147ce1ffb0
+# ╟─92639f81-e406-4a55-8812-66ec01d29b16
+# ╟─b0090f60-95d0-4f12-ab6f-6c74a636f686
+# ╟─aa640f8d-791a-4c28-a3e9-c5cfae8ed7e7
+# ╟─8b406501-0c30-40e8-b5fd-dc8912f4a9cb
+# ╟─b58a68aa-4709-4c1f-b680-dfe434679a04
+# ╟─a8e876a7-4881-4b2a-87fd-6471c35dffd9
+# ╟─9e431fda-2f68-48dd-8d6d-ea08f0eb28bc
+# ╟─2dd5b6a3-8146-460d-9dd1-31c34bf6e2f8
+# ╟─5c4ed95d-0119-4d06-b6b2-378169029bcd
+# ╟─00982b72-a1e4-4ce0-8fc7-1d147ce1ffb0
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
